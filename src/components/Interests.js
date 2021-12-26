2b089490fd5547c9f7da10bf54cf86afe4122993
LOKESH <lokeshkhati005@gmail.com>
2021-12-26 14:41:26 +0530

    Update Interests.js

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -4,7 +4,7 @@ export default function Interests(){
     return(
         <section className="interest-section">
             <h1 className="interest-heading">Interests</h1>
-            <p className="interest-text"> Food expert (Khichdi), Reader, Internet fanatic, Tech entusiastic, Coffee fanatic</p>
+            <p className="interest-text"> Food expert (Khichdi), Reader, Internet fanatic, Tech enthusiastic, Coffee fanatic</p>
         </section>
     )
 }
